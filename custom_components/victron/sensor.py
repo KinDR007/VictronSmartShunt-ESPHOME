4134b3a4e3c5b014f87b611d81a4ceba7a2c4a02
Sebastian Muszynski <s.muszynski@constructiva.de>
2021-09-05 19:56:16 +0200

    Apply black format

--- custom_components/victron/sensor.py
+++ custom_components/victron/sensor.py
@@ -1,15 +1,32 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, text_sensor, uart
-from esphome.const import CONF_ID, UNIT_VOLT, ICON_FLASH, UNIT_AMPERE, UNIT_WATT, \
-    UNIT_WATT_HOURS, ICON_POWER, ICON_CURRENT_AC, ICON_COUNTER, UNIT_EMPTY, ICON_EMPTY, \
-    DEVICE_CLASS_POWER, DEVICE_CLASS_CURRENT, DEVICE_CLASS_VOLTAGE, DEVICE_CLASS_EMPTY, \
-    UNIT_PERCENT, ICON_PERCENT,ICON_TIMELAPSE ,UNIT_MINUTE
+from esphome.const import (
+    CONF_ID,
+    UNIT_VOLT,
+    ICON_FLASH,
+    UNIT_AMPERE,
+    UNIT_WATT,
+    UNIT_WATT_HOURS,
+    ICON_POWER,
+    ICON_CURRENT_AC,
+    ICON_COUNTER,
+    UNIT_EMPTY,
+    ICON_EMPTY,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_VOLTAGE,
+    DEVICE_CLASS_EMPTY,
+    UNIT_PERCENT,
+    ICON_PERCENT,
+    ICON_TIMELAPSE,
+    UNIT_MINUTE,
+)
 from . import victron_ns, VictronComponent, CONF_VICTRON_ID
 
 
 # sensors SS
-CONF_INSTANTENEOUS_POWER = "instantaneous_power" #okamžitý výkon
+CONF_INSTANTENEOUS_POWER = "instantaneous_power"  # okamžitý výkon
 CONF_TIME_TO_GO = "time_to_go"
 CONF_STATE_OF_CHARGE = "state_of_charge"
 CONF_CONSUMED_AMP_HOUR = "consumed_amp_hours"
@@ -20,9 +37,8 @@ CONF_BMV_ALARM_TEXT = "bmv_alarm"
 CONF_BMV_TEXT = "bmv_pid"
 CONF_LAST_FULL_CHARGE = "last_full_charge"
 CONF_DEEPEST_DISCHAGRE = "deepest_discharge"
-CONF_LAST_DISCHARGE =  "last_discharge"
-CONF_DISCHARGED_ENERGY =  "discharged_energy" 
- 
+CONF_LAST_DISCHARGE = "last_discharge"
+CONF_DISCHARGED_ENERGY = "discharged_energy"
 
 
 # sensors
@@ -48,44 +64,107 @@ CONF_TRACKER_TEXT = "tracker_text"
 CONF_FW_VERSION = "fw_version"
 CONF_PID = "pid"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_VICTRON_ID): cv.use_id(VictronComponent),
-    cv.Optional(CONF_MAX_POWER_YESTERDAY): sensor.sensor_schema(UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_MAX_POWER_TODAY): sensor.sensor_schema(UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_YIELD_TOTAL): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_YIELD_YESTERDAY): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_YIELD_TODAY): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_PANEL_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE),
-    cv.Optional(CONF_PANEL_POWER): sensor.sensor_schema(UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE),
-    cv.Optional(CONF_BATTERY_CURRENT): sensor.sensor_schema(UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT),
-    cv.Optional(CONF_DAY_NUMBER): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_CHARGER_STATUS): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_ERROR_CODE): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_TRACKER_OPERATION): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_LOAD_CURRENT): sensor.sensor_schema(UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT),
-    cv.Optional(CONF_CHARGER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_ERROR_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_TRACKER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_FW_VERSION): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_PID): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_INSTANTENEOUS_POWER): sensor.sensor_schema(UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_TIME_TO_GO): sensor.sensor_schema(UNIT_MINUTE, ICON_TIMELAPSE, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_STATE_OF_CHARGE): sensor.sensor_schema(UNIT_PERCENT, ICON_PERCENT, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_CONSUMED_AMP_HOUR): sensor.sensor_schema(UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_BMV_ALARM_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_BMV_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend({cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}),
-    cv.Optional(CONF_MIN_BATTERY_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE),
-    cv.Optional(CONF_MAX_BATTERY_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE),
-    cv.Optional(CONF_AMOUNT_OF_CHARGED): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER),
-    cv.Optional(CONF_LAST_FULL_CHARGE): sensor.sensor_schema(UNIT_MINUTE, ICON_TIMELAPSE, 0, DEVICE_CLASS_EMPTY),
-    cv.Optional(CONF_DEEPEST_DISCHAGRE): sensor.sensor_schema(UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT),
-    cv.Optional(CONF_LAST_DISCHARGE): sensor.sensor_schema(UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT),
-    cv.Optional(CONF_DISCHARGED_ENERGY): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER)
-})
-
-
-
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_VICTRON_ID): cv.use_id(VictronComponent),
+        cv.Optional(CONF_MAX_POWER_YESTERDAY): sensor.sensor_schema(
+            UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_MAX_POWER_TODAY): sensor.sensor_schema(
+            UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_YIELD_TOTAL): sensor.sensor_schema(
+            UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_YIELD_YESTERDAY): sensor.sensor_schema(
+            UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_YIELD_TODAY): sensor.sensor_schema(
+            UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_PANEL_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_PANEL_POWER): sensor.sensor_schema(
+            UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_BATTERY_CURRENT): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_DAY_NUMBER): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_CHARGER_STATUS): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_ERROR_CODE): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_TRACKER_OPERATION): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_LOAD_CURRENT): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_CHARGER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_ERROR_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_TRACKER_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_FW_VERSION): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_PID): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_INSTANTENEOUS_POWER): sensor.sensor_schema(
+            UNIT_WATT, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_TIME_TO_GO): sensor.sensor_schema(
+            UNIT_MINUTE, ICON_TIMELAPSE, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_STATE_OF_CHARGE): sensor.sensor_schema(
+            UNIT_PERCENT, ICON_PERCENT, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_CONSUMED_AMP_HOUR): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_BMV_ALARM_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_BMV_TEXT): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {cv.GenerateID(): cv.declare_id(text_sensor.TextSensor)}
+        ),
+        cv.Optional(CONF_MIN_BATTERY_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_MAX_BATTERY_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_FLASH, 3, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_AMOUNT_OF_CHARGED): sensor.sensor_schema(
+            UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_LAST_FULL_CHARGE): sensor.sensor_schema(
+            UNIT_MINUTE, ICON_TIMELAPSE, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_DEEPEST_DISCHAGRE): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_LAST_DISCHARGE): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_CURRENT_AC, 3, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_DISCHARGED_ENERGY): sensor.sensor_schema(
+            UNIT_WATT_HOURS, ICON_POWER, 0, DEVICE_CLASS_POWER
+        ),
+    }
+)
 
 
 def to_code(config):
@@ -129,7 +208,7 @@ def to_code(config):
 
     if CONF_LOAD_CURRENT in config:
         sens = yield sensor.new_sensor(config[CONF_LOAD_CURRENT])
-        cg.add(victron.set_load_current_sensor(sens))    
+        cg.add(victron.set_load_current_sensor(sens))
 
     if CONF_DAY_NUMBER in config:
         sens = yield sensor.new_sensor(config[CONF_DAY_NUMBER])
@@ -188,11 +267,11 @@ def to_code(config):
     if CONF_STATE_OF_CHARGE in config:
         sens = yield sensor.new_sensor(config[CONF_STATE_OF_CHARGE])
         cg.add(victron.set_state_of_charge_sensor(sens))
-    
+
     if CONF_CONSUMED_AMP_HOUR in config:
         sens = yield sensor.new_sensor(config[CONF_CONSUMED_AMP_HOUR])
         cg.add(victron.set_consumed_amp_hours_sensor(sens))
-#**************************** SS***************
+    # **************************** SS***************
     if CONF_BMV_ALARM_TEXT in config:
         conf = config[CONF_BMV_ALARM_TEXT]
         sens = cg.new_Pvariable(conf[CONF_ID])
@@ -203,7 +282,7 @@ def to_code(config):
         conf = config[CONF_BMV_TEXT]
         sens = cg.new_Pvariable(conf[CONF_ID])
         yield text_sensor.register_text_sensor(sens, conf)
-        cg.add(victron.set_bmv_sensor(sens))  
+        cg.add(victron.set_bmv_sensor(sens))
 
     if CONF_AMOUNT_OF_CHARGED in config:
         sens = yield sensor.new_sensor(config[CONF_AMOUNT_OF_CHARGED])
@@ -211,26 +290,24 @@ def to_code(config):
 
     if CONF_MIN_BATTERY_VOLTAGE in config:
         sens = yield sensor.new_sensor(config[CONF_MIN_BATTERY_VOLTAGE])
-        cg.add(victron.set_min_battery_voltage_sensor(sens))   
+        cg.add(victron.set_min_battery_voltage_sensor(sens))
 
     if CONF_MAX_BATTERY_VOLTAGE in config:
         sens = yield sensor.new_sensor(config[CONF_MAX_BATTERY_VOLTAGE])
-        cg.add(victron.set_max_battery_voltage_sensor(sens))   
+        cg.add(victron.set_max_battery_voltage_sensor(sens))
 
-               
     if CONF_LAST_FULL_CHARGE in config:
         sens = yield sensor.new_sensor(config[CONF_LAST_FULL_CHARGE])
-        cg.add(victron.set_last_full_charge_sensor(sens))  
-               
+        cg.add(victron.set_last_full_charge_sensor(sens))
+
     if CONF_DEEPEST_DISCHAGRE in config:
         sens = yield sensor.new_sensor(config[CONF_DEEPEST_DISCHAGRE])
-        cg.add(victron.set_depth_deepest_dis_sensor(sens))          
-               
+        cg.add(victron.set_depth_deepest_dis_sensor(sens))
+
     if CONF_LAST_DISCHARGE in config:
         sens = yield sensor.new_sensor(config[CONF_LAST_DISCHARGE])
-        cg.add(victron.set_depth_of_the_last_discharge_sensor(sens))  
-               
+        cg.add(victron.set_depth_of_the_last_discharge_sensor(sens))
+
     if CONF_DISCHARGED_ENERGY in config:
         sens = yield sensor.new_sensor(config[CONF_DISCHARGED_ENERGY])
-        cg.add(victron.set_amount_of_discharged_energy_sensor(sens))  
-
+        cg.add(victron.set_amount_of_discharged_energy_sensor(sens))
