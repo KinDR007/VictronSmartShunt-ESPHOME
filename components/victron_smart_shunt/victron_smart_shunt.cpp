1ae5a51189e62f9418c762d15153b5d35001ae59
Yoann Chaudet <yoannchaudet@github.com>
2022-12-24 19:48:36 -0800

    more typos

--- components/victron_smart_shunt/victron_smart_shunt.cpp
+++ components/victron_smart_shunt/victron_smart_shunt.cpp
@@ -494,9 +494,9 @@ void VictronSmartShuntComponent::handle_value_() {
     }
   } else if (label_ == "T") {
     if (value_ == "---") {
-      battery_temperature_sensor_->publish_state_(NAN);
+      battery_temperature_sensor_->publish_state(NAN);
     } else {
-      battery_temperature_sensor_->publish_state_(atoi(value_.c_str()));  // NOLINT(cert-err34-c)
+      battery_temperature_sensor_->publish_state(atoi(value_.c_str()));  // NOLINT(cert-err34-c)
     }
   }
   } else {
